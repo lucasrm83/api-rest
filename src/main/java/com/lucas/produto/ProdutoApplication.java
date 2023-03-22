41ee366dad2aa2c8cb234ce6f8af6e34d8f1dfff
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-03-22 09:04:27 -0300

    product, productresources e productrepository adicionados

--- src/main/java/com/lucas/produto/ProdutoApplication.java
+++ src/main/java/com/lucas/produto/ProdutoApplication.java
@@ -1,16 +1,22 @@
 package com.lucas.produto;
 
 import com.lucas.produto.entities.Category;
+import com.lucas.produto.entities.Product;
 import com.lucas.produto.repositories.CategoryRepository;
+import com.lucas.produto.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class ProdutoApplication implements CommandLineRunner {
     @Autowired
     private CategoryRepository categoryRepository;
+	@Autowired
+	private ProductRepository productRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProdutoApplication.class, args);
@@ -20,7 +26,18 @@ public class ProdutoApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Category cat1 = new Category(1L, "Eletronics");
 		Category cat2 = new Category(2L, "Books");
+
+		Product p1 = new Product(1L,"TV",2200.00,cat1);
+		Product p2 = new Product(2L,"Dmain Driven Design",120.00,cat2);
+		Product p3 = new Product(3L,"PS5",2800.00,cat1);
+		Product p4 = new Product(4L,"Docker",100.00,cat1);
+		cat1.getProducts().addAll(Arrays.asList(p1,p3));
+		cat2.getProducts().addAll(Arrays.asList(p2,p4));
         categoryRepository.save(cat1);
         categoryRepository.save(cat2);
+		productRepository.save(p1);
+		productRepository.save(p2);
+		productRepository.save(p3);
+		productRepository.save(p4);
 	}
 }
